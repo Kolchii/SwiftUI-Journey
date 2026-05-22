ed17d5ea9265ce475f5499293e6f20093d94db83
Kolchi <kolchiibrahim@icloud.com>
2026-05-22 16:04:00 +0400

    Add initial SwiftUI project scaffold

---
+++ SwiftUI-Journey/SwiftUI_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JourneyApp.swift
+//  SwiftUI-Journey
+//
+//  Created by Ibrahim Kolchi on 22.05.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
