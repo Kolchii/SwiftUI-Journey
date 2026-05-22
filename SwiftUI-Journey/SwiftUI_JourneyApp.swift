1cb726e6e96e130fda9cb497e0a9f987ede240dc
Kolchi <kolchiibrahim@icloud.com>
2026-05-22 16:07:45 +0400

    .

--- SwiftUI-Journey/SwiftUI_JourneyApp.swift
+++ SwiftUI-Journey/SwiftUI_JourneyApp.swift
@@ -4,14 +4,3 @@
 //
 //  Created by Ibrahim Kolchi on 22.05.26.
 //
-
-import SwiftUI
-
-@main
-struct SwiftUI_JourneyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
